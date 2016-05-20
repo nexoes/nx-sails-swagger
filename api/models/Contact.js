69592827bd7c55b6c614b8766c15aeb079e7808b
Peter Andreas Moelgaard <admin@petermolgaard.com>
2016-05-20 16:57:56 +0800

    merged with content from NPM

--- api/models/Contact.js
+++ api/models/Contact.js
@@ -1,31 +1,17 @@
 /**
- * Contact.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/#!documentation/models
- */
+* Contact.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
 
 module.exports = {
 
-    testInstance() {
-        return {
-        	name: 'Contact Name',
-        	group: [
-                 {
-                 	name: 'Group Name'
-                 }
-        	]
-        }
-    },
-
-    attributes: {
-        name: {
-            type: 'string',
-            defaultsTo: 'Contact Name'
-        },
-        group: {
-            model: 'Group'
-        }
-
+  attributes: {
+    group: {
+      model: 'Group'
     }
+
+  }
 };
+
